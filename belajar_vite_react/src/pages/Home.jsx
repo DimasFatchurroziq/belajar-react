eb3f2083ddcf562ef7b36bb62b9fe1ee3fd8980a
DimasFatchurroziq <dimasfatchurroziq.com>
2025-03-10 15:30:41 +0700

    New Section

--- belajar_vite_react/src/pages/Home.jsx
+++ belajar_vite_react/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Tutors from '../components/Tutors'
+import Partners from '../components/Partners'
+import Contact from '../components/Contact'
+
 import '../styles/Home.css'
-import { homeSection } from '../datas/HomeSection'
+
+import { homeSection } from '../data/HomeSection'
+import { courseSection } from '../data/CourseSection'
+import { tutorsSection, tutorsList } from '../data/TutorsSection'
+import { partnersSection, partnersList } from '../data/PartnersSection'
+import { contactSection } from '../data/ContactSection'
 
 import parse from 'html-react-parser'
 
@@ -10,14 +19,42 @@ function Home() {
   return (
     <div>
       <Navbar />
-        <div className='wrapper'>
-            <section id='Home'>
-                <img src={homeSection.image} alt="" />
-                <div className='kolom'>
-                    {parse(homeSection.content)}
-                </div>
-            </section>
-        </div>
+      <div className='wrapper'>
+
+        {/* home */}
+        <section id='Home'>
+          <img src={homeSection.image} alt="" />
+          <div className='kolom'>
+            {parse(homeSection.content)}
+          </div>
+        </section>
+
+        {/* course */}
+        <section id="courses">
+          <div className="kolom">
+            {parse(courseSection.content)}
+          </div>
+          <img src={courseSection.image} alt="" />
+        </section>
+
+        {/* tutors */}
+        <section id="tutors">
+            <div className="tengah">
+              {parse(tutorsSection.content)}
+              <Tutors tutorsList={tutorsList}/>
+            </div>
+        </section>
+
+        {/* partners */}
+        <section id="partners">
+          <div className="tengah">
+            {parse(partnersSection.content)}
+            <Partners partnersList={partnersList}/>
+          </div>
+        </section>
+      
+      </div>
+      <Contact contactSection={contactSection}/>
       <Footer />
     </div>
   )
